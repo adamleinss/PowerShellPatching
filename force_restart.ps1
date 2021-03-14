064420bab909385bee1ac53bcc6cacc6f3d9b3f3
Adam Leinss <35347500+adamleinss@users.noreply.github.com>
2021-03-14 11:16:21 -0500

    Add files via upload

---
+++ force_restart.ps1
@@ -0,0 +1,10 @@
+﻿$ServersPatching = Get-Content D:\cron\PatchAdams\servers.txt
+
+$servers = @('SOMESRVER')
+
+For ($i=0; $i -lt $servers.Length; $i++) {
+  if ($ServersPatching | Select-String -Pattern $servers[$i])
+      { Write-Host Restarting $servers[$i]
+        Restart-Computer $servers[$i] -Force
+        } # end if
+ } #end for
