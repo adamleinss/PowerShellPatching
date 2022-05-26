6affc77c8267866274eeafff6ff7f8a4fd17f337
Adam Leinss <35347500+adamleinss@users.noreply.github.com>
2022-05-26 18:29:13 -0500

    Add files via upload

--- PatchReportSMTP.ps1
+++ PatchReportSMTP.ps1
@@ -18,12 +18,13 @@ $found2012r2 = "FALSE"
 $found2012 = "FALSE"
 $found2016 = "FALSE"
 $found2019 = "FALSE"
+$found2022 = "FALSE"
 
 For ($i=0; $i -lt $rslt.Length; $i++) {
 #write-output $rslt[$i]
-    if (($rslt[$i] | select-string -pattern 'Windows Server 2012 R2') -and  ($rslt[$i] | select-string -pattern $pattern_no_kb_prefix) -and $found2012r2 -eq "FALSE" ) {
+    if (($rslt[$i] | select-string -pattern 'Windows Server 2012 R2') -and  ($rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix) -and $found2012r2 -eq "FALSE" ) {
    # write-output $rslt[$i]
-       $2012R2 = $rslt[$i] | select-string -pattern $pattern_no_kb_prefix
+       $2012R2 = $rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix
        $KB2012R2 = "KB" + $2012R2.Matches
        $patchz += $KB2012R2
        $found2012r2 = "TRUE"
@@ -31,15 +32,15 @@ For ($i=0; $i -lt $rslt.Length; $i++) {
     
     } #end if
 
-if (($rslt[$i] | select-string -pattern 'Windows Server 2012') -and  ($rslt[$i] | select-string -pattern $pattern_no_kb_prefix) -and $found2012 -eq "FALSE" ) {
-       $2012 = $rslt[$i] | select-string -pattern $pattern_no_kb_prefix
+if (($rslt[$i] | select-string -pattern 'Windows Server 2012') -and  ($rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix) -and $found2012 -eq "FALSE" ) {
+       $2012 = $rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix
        $KB2012 = "KB" + $2012.Matches  
 	   
 	   if ($KB2012 -ne $KB2012R2) {
 	   
 			$patchz += $KB2012
 			$found2012 = "TRUE"
-			write-host $KB2012
+			#write-host $KB2012
 	   } #end dup if
 	          
     } #end if
@@ -47,7 +48,7 @@ if (($rslt[$i] | select-string -pattern 'Windows Server 2012') -and  ($rslt[$i] 
 
 if (($rslt[$i] | select-string -pattern 'Windows Server 2016') -and $found2016 -eq "FALSE" ) {
 
-       $2016 = $rslt[$i] | select-string -pattern $kbpattern
+       $2016 = $rslt[$i+1] | select-string -pattern $kbpattern
        $KB2016 = $2016.Matches
        $patchz += $KB2016
        $found2016 = "TRUE"
@@ -57,7 +58,7 @@ if (($rslt[$i] | select-string -pattern 'Windows Server 2016') -and $found2016 -
 
 if (($rslt[$i] | select-string -pattern 'Windows Server 2019') -and $found2019 -eq "FALSE" ) {
 
-       $2019 = $rslt[$i] | select-string -pattern $kbpattern
+       $2019 = $rslt[$i+1] | select-string -pattern $kbpattern
        $KB2019 = $2019.Matches
        $patchz += $KB2019
        $found2019 = "TRUE"
@@ -65,9 +66,21 @@ if (($rslt[$i] | select-string -pattern 'Windows Server 2019') -and $found2019 -
     
     } #end if
 
+
+
+if (($rslt[$i] | select-string -pattern 'Windows Server 2022') -and $found2022 -eq "FALSE" ) {
+
+       $2022 = $rslt[$i+1] | select-string -pattern $kbpattern
+       $KB2022 = $2022.Matches
+       $patchz += $KB2022
+       $found2022 = "TRUE"
+       #write-host $KB2022
+    
+    } #end if
+
 } #end for
 
-$patchlist = "$($Patchz[0])","$($Patchz[1])", "$($Patchz[2])", "$($Patchz[3])"
+$patchlist = "$($Patchz[0])","$($Patchz[1])", "$($Patchz[2])", "$($Patchz[3])", "$($Patchz[4])"
 write-host $patchlist
 
 Invoke-Command -ComputerName $servers {
@@ -88,6 +101,6 @@ Stop-Transcript
 
 $patchreport = get-content D:\CRON\PatchAdams\patchreport.txt -Raw
 
-Send-MailMessage -From 'patchreport@acme.com' -To 'boss@acme.com' -Subject 'Patch Compliance Report' -Body "
-$patchreport" -SmtpServer '127.0.0.1'
+Send-MailMessage -From 'patchreport@milwaukee.k12.wi.us' -To 'leinssam@milwaukee.k12.wi.us' -Subject 'Patch Compliance Report' -Body "
+$patchreport" -SmtpServer '10.8.64.101'
 
