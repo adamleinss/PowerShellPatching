ac562b8981c73fe3384c7b7ec3df00739219fa6e
Adam Leinss <35347500+adamleinss@users.noreply.github.com>
2022-06-23 13:21:10 -0500

    Add files via upload

---
+++ Get-SecurityUpdateAL.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module MSRCSecurityUpdates -Force
+
+$date = Get-Date -Format "yyyy-MMM"
+$datestr = $date.ToString()
+
+$rslt = Get-MsrcCvrfDocument -ID $datestr | Get-MsrcCvrfAffectedSoftware
+
+$rslt | out-file -Width 300 D:\cron\patchadams\patches.txt
