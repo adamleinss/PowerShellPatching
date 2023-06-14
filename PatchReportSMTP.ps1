ae904115fd367c838fad222ee02cf992ff0e657f
Adam Leinss <35347500+adamleinss@users.noreply.github.com>
2023-06-14 07:22:37 -0500

    Update PatchReportSMTP.ps1

--- PatchReportSMTP.ps1
+++ PatchReportSMTP.ps1
@@ -1,4 +1,4 @@
-﻿Stop-Transcript | out-null
+Stop-Transcript | out-null
 Start-Transcript -path D:\cron\patchadams\patchreport.txt
 
 $Credential = Import-CliXml -Path D:\cron\PatchAdams\bsod.cred
@@ -9,83 +9,51 @@ $servers = get-content D:\cron\patchadams\servers.txt
 
 $rslt = get-content D:\cron\patchadams\patches.txt
 
-$patchz = @()
+# Specify the target FullProductNames
+$targetProductNames = "Windows Server 2012", "Windows Server 2012 R2", "Windows Server 2016", "Windows Server 2019", "Windows Server 2022"
 
-$kbpattern = 'KB\d\d\d\d\d\d\d'
-$pattern_no_kb_prefix = '\d\d\d\d\d\d\d'
+# HashSet to store the unique KB numbers
+$kbNumbers = [System.Collections.Generic.HashSet[string]]::new()
 
-$found2012r2 = "FALSE"
-$found2012 = "FALSE"
-$found2016 = "FALSE"
-$found2019 = "FALSE"
-$found2022 = "FALSE"
+# Flag to indicate if the current line contains a FullProductName
+$foundProductName = $false
 
-For ($i=0; $i -lt $rslt.Length; $i++) {
-#write-output $rslt[$i]
-    if (($rslt[$i] | select-string -pattern 'Windows Server 2012 R2') -and  ($rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix) -and $found2012r2 -eq "FALSE" ) {
-   # write-output $rslt[$i]
-       $2012R2 = $rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix
-       $KB2012R2 = "KB" + $2012R2.Matches
-       $patchz += $KB2012R2
-       $found2012r2 = "TRUE"
-       #write-host $KB2012R2
-    
-    } #end if
+# Iterate through each line in the file
+foreach ($line in $rslt) {
+    # Check if the line contains FullProductName
+    if ($line -like "FullProductName*") {
+        $productName = ($line -split ":")[1].Trim()
 
-if (($rslt[$i] | select-string -pattern 'Windows Server 2012') -and  ($rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix) -and $found2012 -eq "FALSE" ) {
-       $2012 = $rslt[$i+1] | select-string -pattern $pattern_no_kb_prefix
-       $KB2012 = "KB" + $2012.Matches  
-	   
-	   if ($KB2012 -ne $KB2012R2) {
-	   
-			$patchz += $KB2012
-			$found2012 = "TRUE"
-			#write-host $KB2012
-	   } #end dup if
-	          
-    } #end if
+        # Check if the FullProductName matches the target names
+        if ($targetProductNames -contains $productName) {
+            $foundProductName = $true
+        } else {
+            $foundProductName = $false
+        }
+    }
 
+    # Check if the line contains KBArticle
+    if ($line -like "KBArticle*") {
+        if ($foundProductName) {
+            # Extract the KB numbers from the KBArticle field
+            $kbMatches = [regex]::Matches($line, "ID=(\d{7})")
 
-if (($rslt[$i] | select-string -pattern 'Windows Server 2016') -and $found2016 -eq "FALSE" ) {
+            # Iterate through each match and append "KB" to the front of it
+            foreach ($match in $kbMatches) {
+                $kbNumber = "KB" + $match.Groups[1].Value
+                $kbNumbers.Add($kbNumber)
+            }
+        }
+    }
+}
 
-       $2016 = $rslt[$i+1] | select-string -pattern $kbpattern
-       $KB2016 = $2016.Matches
-       $patchz += $KB2016
-       $found2016 = "TRUE"
-       #write-host $KB2016
-    
-    } #end if
+# Output the array of unique KB numbers
+$kbNumbers
 
-if (($rslt[$i] | select-string -pattern 'Windows Server 2019') -and $found2019 -eq "FALSE" ) {
-
-       $2019 = $rslt[$i+1] | select-string -pattern $kbpattern
-       $KB2019 = $2019.Matches
-       $patchz += $KB2019
-       $found2019 = "TRUE"
-       #write-host $KB2019
-    
-    } #end if
-
-
-
-if (($rslt[$i] | select-string -pattern 'Windows Server 2022') -and $found2022 -eq "FALSE" ) {
-
-       $2022 = $rslt[$i+1] | select-string -pattern $kbpattern
-       $KB2022 = $2022.Matches
-       $patchz += $KB2022
-       $found2022 = "TRUE"
-       #write-host $KB2022
-    
-    } #end if
-
-} #end for
-
-$patchlist = "$($Patchz[0])","$($Patchz[1])", "$($Patchz[2])", "$($Patchz[3])", "$($Patchz[4])"
-write-host $patchlist
 
 Invoke-Command -ComputerName $servers {
 	$FormatEnumerationLimit=-1
-    Get-HotFix -Id $using:patchlist | Format-Table -Wrap | Out-String -Width 300
+    Get-HotFix -Id $using:kbNumbers | Format-Table -Wrap | Out-String -Width 300
 } -Credential ($Credential) -ErrorAction SilentlyContinue -ErrorVariable Problem
  
 foreach ($p in $Problem) {
@@ -93,7 +61,7 @@ foreach ($p in $Problem) {
         Write-Warning -Message "Patch not found on $($p.origininfo.pscomputername)" 
     }
     elseif ($p.targetobject) {
-      #  Write-Warning -Message "Unable to connect to $($p.targetobject)"
+       # Write-Warning -Message "Unable to connect to $($p.targetobject)"
     }
 }
 
@@ -101,6 +69,7 @@ Stop-Transcript
 
 $patchreport = get-content D:\CRON\PatchAdams\patchreport.txt -Raw
 
-Send-MailMessage -From 'patchreport@acme.com' -To 'daboss@acem.com' -Subject 'Patch Compliance Report' -Body "
+Send-MailMessage -From 'patchreport@acme.com' -To 'bob@acme.com' -Subject 'Patch Compliance Report' -Body "
 $patchreport" -SmtpServer '127.0.0.1'
 
+#>
